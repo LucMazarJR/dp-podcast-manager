0ccd5a8ab4cf2e5d6af1ba4e6856d244092e1499
LucianoMazar <lmazaraojr@gmail.com>
2025-07-17 11:46:32 -0300

    Começo da implementação do servies

---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,12 @@
+export const serviceListEpisodes = async () => {
+    const data = [
+        {
+            podcastName: "",
+            episode: "",
+            videoId: "",
+            categories: []
+        }
+    ]
+
+    return data
+}
