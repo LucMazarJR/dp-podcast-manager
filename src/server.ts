e5b79751af64dbb853bc0046693db289cdf3adc4
LucianoMazar <lmazaraojr@gmail.com>
2025-07-18 10:04:31 -0300

    Refatorando o projeto

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,10 @@
 import * as http from "http";
-import {getFilterEpisodes, getListEpisodes} from "./controllers/podscasts-controller"
+import { app } from "./app";
 
-const server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    const [baseUrl, queryString] = req.url?.split("?") ?? ["", ""]
-
-    if(req.method === "GET" && req.url === "/api/list"){
-        await getListEpisodes(req, res)
-    }
-    if(req.method === "GET" && req.url === "/api/episode"){
-        await getFilterEpisodes(req, res)
-    }
-})
+const server = http.createServer(app)
 
 const port = process.env.PORT
+
 server.listen(port, ()=>{
     console.log(`Servidor iniciado na porta ${port}`)
 })
