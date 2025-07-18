b7e584d6adc819278c543640e6230a338b3a2362
LucianoMazar <lmazaraojr@gmail.com>
2025-07-18 10:31:26 -0300

    Tratamento de erros

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,21 +1,18 @@
 import {IncomingMessage, ServerResponse} from 'http';
 import {serviceListEpisodes} from "../services/list-episodes-service"
 import {serviceFilterEpisodes} from "../services/filter-episodes-service"
-import { StatusCode } from '../utils/status-code';
 import { ContentType } from '../utils/content-type';
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
     const content = await serviceListEpisodes()
-    res.writeHead(StatusCode.OK, {'Content-Type': ContentType.JSON});
-    res.end(
-        JSON.stringify(content)
-    )
+    res.writeHead(content.statusCode, {'Content-Type': ContentType.JSON});
+    res.write(JSON.stringify(content.body))
+    res.end()
 }
 
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
     const content = await serviceFilterEpisodes(req.url)
-    res.writeHead(StatusCode.OK, {'Content-Type': ContentType.JSON});
-    res.end(
-        JSON.stringify(content)
-    )
+    res.writeHead(content.statusCode, {'Content-Type': ContentType.JSON});
+    res.write(JSON.stringify(content.body))
+    res.end()
 }
--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,9 +1,18 @@
 import { IncomingMessage } from "http"
 import { repoPodcast } from "../repositories/podcasts-repository"
+import { PodcastTransferModel } from "../models/Podcast-Transfer-Model"
+import { StatusCode } from "../utils/status-code"
 
-export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
+export const serviceFilterEpisodes = async (podcastName: string | undefined) : Promise<PodcastTransferModel>=> {
+    let responseFormat:PodcastTransferModel = {
+        statusCode: 0,
+        body: [] 
+    }
+
     const queryString = podcastName?.split("?p=")[1] || ""
     const data = await repoPodcast(queryString)
 
-    return data
+    responseFormat.statusCode = data.length !== 0 ? StatusCode.OK : StatusCode.NO_CONTENT
+
+    return responseFormat
 }
